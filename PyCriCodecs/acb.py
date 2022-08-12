c528259292dcaecf243835cdab016250bfc43498
LittleChungi <110078250+LittleChungi@users.noreply.github.com>
2022-08-12 16:21:50 +1200

    ACB extraction improvement.

--- PyCriCodecs/acb.py
+++ PyCriCodecs/acb.py
@@ -5,12 +5,14 @@ import os
 
 class ACB(UTF):
     """ An ACB is basically a giant @UTF table. Use this class to extract any ACB. """
-    __slots__ = ["filename", "payload", "dirname"]
+    __slots__ = ["filename", "payload", "dirname", "filename"]
     payload: list[dict]
     dirname: str
+    filename: str
 
     def __init__(self, filename, dirname: str = "") -> None:
         self.payload = UTF(filename).get_payload()
+        self.filename = filename
         self.acbparse(self.payload)
         self.dirname = dirname
         # TODO check on ACB version.
@@ -29,7 +31,7 @@ class ACB(UTF):
         # There are two types of ACB's, one that has an AWB file inside it,
         # and one with an AWB pair.
         if self.payload[0]['AwbFile'][1] == b'':
-            awbObj = AWB(self.payload[0]['Name'][1]+".awb")
+            awbObj = AWB(os.path.join(os.path.dirname(self.filename), self.payload[0]['Name'][1]+".awb"))
         else:
             awbObj = AWB(self.payload[0]['AwbFile'][1])
         

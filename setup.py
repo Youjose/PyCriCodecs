6a4d13db5ae1e2558113a8ab91af199349b0eac4
LittleChungi <110078250+LittleChungi@users.noreply.github.com>
2022-09-21 22:43:39 +1200

    Tweak.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="PyCriCodecs",
-    version="0.1.5",
+    version="0.1.6",
     description="Python frontend with a C++ backend of managing Criware files of all kind.",
     packages=["PyCriCodecs"],
     ext_modules=[Extension('PyCriCodecs.CriCodecs', ["CriCodecs\\CriCodecs.cpp"], include_dirs=[os.path.realpath("CriCodecs")])]
